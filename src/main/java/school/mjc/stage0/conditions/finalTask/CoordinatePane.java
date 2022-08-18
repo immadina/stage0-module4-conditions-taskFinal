7de8d3fab3a6d56482d97af48a600defdb52cbef
madina <91madina@gmail.com>
2022-08-19 00:23:56 +0600

    Module #4, Task final

--- src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class CoordinatePane {
     public void printQuadrant(int x, int y) {
+        //"first", "second", "third", "fourth", "zero"
+        if (x == 0 || y == 0) {
+            System.out.println("zero");
+        } else if (x > 0 && y > 0) {
+            System.out.println("first");
+        } else if (x < 0 && y > 0) {
+            System.out.println("second");
+        } else if (x < 0 && y < 0) {
+            System.out.println("third");
+        } else {
+            System.out.println("fourth");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class TriangleSidesValidator {
     public void validate(double firstSide, double secondSide, double thirdSide) {
-
+//"this is a valid triangle", otherwise : "it's not a triangle"
+        if (firstSide <= 0 || secondSide <= 0 || thirdSide <= 0) {
+            System.out.println("it's not a triangle");
+        } else if ((firstSide + secondSide > thirdSide) && (thirdSide + secondSide > firstSide) && (firstSide + thirdSide > secondSide)){
+            System.out.println("this is a valid triangle");
+        } else {
+            System.out.println("it's not a triangle");
+        }
     }
 }
